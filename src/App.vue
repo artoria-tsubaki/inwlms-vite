c005c9b93bb534fabc61168f960f796d5355c887
pycd <chenwx@gdhmail.com>
2023-04-10 18:01:48 +0800

    install unocss

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="wh-full">
+    <p class="mt-20 text-14 color-gray-400">Flex骰子</p>
+    <div class="f-c-c">
+      <div class="w-100 h-100 bg-blue"></div>
+      <div class="w-100 h-100 bg-green"></div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
